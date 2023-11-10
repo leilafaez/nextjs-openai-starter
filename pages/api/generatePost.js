12700d82564551cad2493775fa48f28880a16000
Shima <shimaadnanbasmenj@gmail.com>
2023-11-10 10:31:51 +0000

    generate the form tag

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -4,25 +4,41 @@ export default async function handler(req, res) {
         apiKey:process.env.OPENAI_API_KEY
     })
     const openai=new OpenAIApi(config);
-    const topic="top 10 tips for dog owners";
-    const keywords="first-time dog owners, common gog health issues, best dog breeds"
+    const{topic,keywords}=req.body
+    // const topic="top 10 tips for dog owners";
+    // const keywords="first-time dog owners, common gog health issues, best dog breeds"
 
-    const response=await openai.createCompletion(
-        {
-                model:"text-davinci-003",
-                temperature:0,
-                max_tokens:3600,
-                prompt:`Write a long and detailed SEO-friendly blog post about ${topic},that targets the following comma-seprated keyword:${keywords}.
-                content should be formatted in SEO-friendly HTML.
-                The respose much include appropriate HTML title and meta description content.
-                The return format must be Stringified JSON in the following format: 
-                {
-                    "PostContent":post content here
-                    "title":title goes here
-                    "metaDescription": meta description goes here
-                } `,
-        }
-    )
-    console.log('response:',response);
-    res.status(200).json({ post:JSON.parse( response.data.choices[0]?.text.split("\n").join("")) })
+    // const response=await openai.createCompletion(
+    //     {
+    //             model:"text-davinci-003",
+    //             temperature:0,
+    //             max_tokens:3600,
+    //             prompt:`Write a long and detailed SEO-friendly blog post about ${topic},that targets the following comma-seprated keyword:${keywords}.
+    //             content should be formatted in SEO-friendly HTML.
+    //             The respose much include appropriate HTML title and meta description content.
+    //             The return format must be Stringified JSON in the following format: 
+    //             {
+    //                 "PostContent":post content here
+    //                 "title":title goes here
+    //                 "metaDescription": meta description goes here
+    //             } `,
+    //     }
+    // )
+    const PostContentResponse=await openai.createChatCompletion({
+        modal:'gpt-3.5-turbo',
+        temperature:0,
+        messages:[{
+            role:"system",
+            content:"you are ablog post generator"
+        },{
+            role:"user",
+            content:`Write a long and detailed SEO-friendly blog post about ${topic},that targets the following comma-seprated keyword:${keywords},
+                         content should be formatted in SEO-friendly HTML,
+                         limited to the followinh HTML tags:p, h1, h2, h3, h4, h5, h6, strong, li, ul, ol, i`
+        }]
+
+    })
+    console.log('response:',PostContentResponse.data.choices[0]?.message?.content);
+    // res.status(200).json({
+    //      post:JSON.parse( response.data.choices[0]?.text.split("\n").join("")) })
   }
--- pages/post/new.js
+++ pages/post/new.js
@@ -4,9 +4,18 @@ import { useState } from "react";
 
 export default function NewPost() {
   const [postContent,setPostContent]=useState('')
-  const handleClick=async()=>{
+  const[topic ,setTopic]=useState('')
+  const[keywords ,setKeywords]=useState('')
+
+  const handleSubmit=async(e)=>{
+          e.preventDefault();
          const response=await fetch(`/api/generatePost`,{
-          method:'POST'
+          method:'POST',
+          headers:{
+            'content-type':'application/json'
+          },
+          body:JSON.stringify(topic,keywords)
+
         });
         const json=await response.json()
         console.log('Result:',json.post.postContent)
@@ -14,10 +23,30 @@ export default function NewPost() {
   }
     return (
     <div>
-      <h1>This is new</h1>
-      <button className="btn" onClick={handleClick}>
+       <form onSubmit={handleSubmit}>
+        <div>
+            <label>
+              <strong>
+                Generate a blog post on topc of:
+              </strong>
+            </label>
+            <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onClick={e=>setTopic(e.target.value)}/>
+        </div>
+        <label>
+              <strong>
+                Targeting the following keyword:
+              </strong>
+            </label>
+            <textarea 
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"value={keywords} onClick={e=>setKeywords (e.target.value)}/>
+        <div>
+          
+        </div>
+            <button type="submit" className="btn" >
           Generate
-      </button>
+            </button>
+       </form>
+      
       <div className="max-w-screen-sm p-10" dangerouslySetInnerHTML={{__html:postContent}}/>
     
     </div>
