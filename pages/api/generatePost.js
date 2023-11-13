54c40592376e4d4ad27863b01696b9a375d1380a
Shima <shimaadnanbasmenj@gmail.com>
2023-11-13 09:55:10 +0000

    set up new turbo modal for gpt

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -38,7 +38,64 @@ export default async function handler(req, res) {
         }]
 
     })
-    console.log('response:',postContentResponse.data.choices[0]?.message?.content);
+    const postContent=postContentResponse.data.choices[0]?.message?.content ||''
+    const titleResponse=await openai.createChatCompletion({
+        modal:'gpt-3.5-turbo',
+        temperature:0,
+        messages:[{
+            role:"system",
+            content:"you are ablog post generator"
+        },{
+            role:"user",
+            content:`Write a long and detailed SEO-friendly blog post about ${topic},that targets the following comma-seprated keyword:${keywords},
+                         content should be formatted in SEO-friendly HTML,
+                         limited to the followinh HTML tags:p, h1, h2, h3, h4, h5, h6, strong, li, ul, ol, i`
+        },
+        {
+           role:"assistant",
+           content:postContent
+        },
+        {
+           role:'user',
+           content:'Generate appropriate title tag text for the above blog post'
+        }]
+
+    })
+    const metaDescriptionResponse=await openai.createChatCompletion({
+        modal:'gpt-3.5-turbo',
+        temperature:0,
+        messages:[{
+            role:"system",
+            content:"you are ablog post generator"
+        },{
+            role:"user",
+            content:`Write a long and detailed SEO-friendly blog post about ${topic},that targets the following comma-seprated keyword:${keywords},
+                         content should be formatted in SEO-friendly HTML,
+                         limited to the followinh HTML tags:p, h1, h2, h3, h4, h5, h6, strong, li, ul, ol, i`
+        },
+        {
+           role:"assistant",
+           content:postContent
+        },
+        {
+           role:'user',
+           content:'Generate SEO-friendly meta description content for the above blog post'
+        }]
+
+    })
+    const title=titleResponse.data.choices[0]?.message?.content || ''
+    const metaDescription=metaDescriptionResponse.data.choices[0]?.message?.content || ''
+     console.log('POST CONTENT:',postContent);
+     console.log('TITLE:',title);
+     console.log('META DESCRIPTION:',metaDescription);
+
+     res.status(200).json  ({
+        post:{
+            postContent,
+            title,
+            metaDescription,
+        },
+     })
     // res.status(200).json({
     //      post:JSON.parse( response.data.choices[0]?.text.split("\n").join("")) })
   }
