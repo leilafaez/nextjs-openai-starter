72c238bdac8f1b9d687d915d1046236ceb503bd8
Shima <shimaadnanbasmenj@gmail.com>
2024-01-04 20:40:58 +0000

    increasing the tokens

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -20,31 +20,21 @@ export default async function handler(req, res) {
   const checkoutSession=await stripe.checkout.sessions.create({
     line_items:lineItems,
     mode:'payment',
-    success_url:`${protocol}${host}/success`
+    success_url:`${protocol}${host}/success`,
+    payment_intent_data:{
+      metadata:{
+        sub:user.sub
+      }
+    },
+    metadata:{
+      sub:user.sub,
+    }
   })
 
 
   console.log("user :",user);  
 
-  const client = await clientPromise;
-  const db = client.db("BlogStandard");
-
-  const userProfile = await db.collection("users").updateOne(
-    {
-      auth0Id: user.sub,
-    },
-    {
-      $inc: {
-        availableTokens: 10,
-      },
-      $setOnInsert: {
-        auth0Id: user.sub,
-      },
-    },
-    {
-        upsert:true,
-    }
-  );
+  
 
   res.status(200).json({ session:checkoutSession});
 }
