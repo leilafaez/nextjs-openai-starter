5f3e2288c162994836e605b2c381291ddc0400ec
leilafaez <leila.faez@gmail.com>
2024-02-05 12:37:57 +0000

    styling success page

--- pages/success.js
+++ pages/success.js
@@ -1,25 +1,27 @@
+
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../components/AppLayout/AppLayout";
 import { getAppProps } from "../utils/getAppProps";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-regular-svg-icons"; 
 
 export default function Success() {
-  
   return (
-    <div>
-      <h1>Thank you for your purchase!</h1>
-      
+    <div className="h-full flex flex-col justify-center items-center">
+      <FontAwesomeIcon icon={faCheckCircle} className="text-7xl text-green-600 mb-4" />
+      <h1 className="text-3xl mb-4 text-green-600">Thank you for your purchase!</h1>
     </div>
   );
 }
- Success.getLayout = function getLayout(page, pageProps) {
-   return <AppLayout {...pageProps}>{page}</AppLayout>;
- };
+
+Success.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
 
 export const getServerSideProps = withPageAuthRequired({
-  async getServerSideProps(ctx){
+  async getServerSideProps(ctx) {
     const props = await getAppProps(ctx);
-    return{
+    return {
       props
     }
   }
